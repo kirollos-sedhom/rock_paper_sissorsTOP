459b603f1d8aa5ba743fa8bce4efae72bdd5ed2a
karlo Ubuntu <kirollosedhom@gmail.com>
2022-10-30 07:40:48 +0200

    added ui

--- index.js
+++ index.js
@@ -2,13 +2,32 @@ let pscore = 0;
 let cscore = 0;
 let choices = ["rock", "paper", "scissors"];
 let errorflag = 0;
+let endgame = 0;
 function getcomputerchoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
-function playround(params) {
+function playround(param) {
+  if (pscore === 5 || cscore === 5) {
+    if (endgame == 0) {
+      endgame = 1;
+      if (pscore > cscore) {
+        p.textContent += "\nPLAYER WINS!!!";
+        return;
+      } else if (cscore > pscore) {
+        p.textContent += "\nAI is Dominating...";
+        return;
+      } else {
+        p.textContent += "\nDraw!";
+        return;
+      }
+    } else {
+      return;
+    }
+  }
   let computer = getcomputerchoice();
-  params = prompt("what is your choice?");
-  switch (params) {
+  // params = prompt("what is your choice?");
+  param = this.textContent;
+  switch (param) {
     case "rock":
       if (computer == "paper") {
         cscore++;
@@ -36,17 +55,35 @@ function playround(params) {
       errorflag = 1;
       break;
   }
-  console.log(
-    `player picked ${params}\ncomputer picked ${computer}\nplayer score: ${pscore}\ncomputer score: ${cscore}`
-  );
+
+  p.textContent = `player picked ${param},computer picked ${computer}  ,\r\nplayer score: ${pscore},\r\ncomputer score: ${cscore}`;
+  p.setAttribute("style", "white-space: pre;");
 }
-for (let i = 0; i < 5; i++) {
-  setTimeout(playround(), 1000);
-}
-if (pscore > cscore) {
-  console.log("PLAYER WINS!!!");
-} else if (cscore > pscore) {
-  console.log("AI is Dominating...");
-} else {
-  console.log("Draw!");
-}
+const mbody = document.createElement("div");
+const p = document.createElement("p");
+const rock = document.createElement("button");
+const paper = document.createElement("button");
+const scissors = document.createElement("button");
+
+rock.textContent = "rock";
+rock.addEventListener("click", playround);
+paper.addEventListener("click", playround);
+scissors.addEventListener("click", playround);
+paper.textContent = "paper";
+scissors.textContent = "scissors";
+
+document.body.appendChild(rock);
+document.body.appendChild(scissors);
+document.body.appendChild(paper);
+document.body.appendChild(mbody);
+document.body.appendChild(p);
+// for (let i = 0; i < 5; i++) {
+//   setTimeout(playround(), 1000);
+// }
+// if (pscore > cscore) {
+//   console.log("PLAYER WINS!!!");
+// } else if (cscore > pscore) {
+//   console.log("AI is Dominating...");
+// } else {
+//   console.log("Draw!");
+// }
